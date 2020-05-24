fedad1e70c585336178742865a41bf53f8946454
bt <biantaobrad@gmail.com>
2020-05-24 21:55:53 +0800

    proptypes

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function(props) {
+const TodoItem =  function(props) {
   return <li onClick={() => {props.handleClick(props.todo.id)}}>{props.todo.name}</li>
 }
+
+TodoItem.propTypes = {
+  todo: PropTypes.object,
+  handleClick: PropTypes.func
+}
+
+export default TodoItem
